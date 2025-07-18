b03ae245d245a8f3f6584a5f593ad0fafd5058b6
Edri <117730224+EdrianaJoy@users.noreply.github.com>
2025-07-18 15:37:15 +0800

    Fix Log In Page

--- mini-blog/resources/views/components/text-input.blade.php
+++ mini-blog/resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-pink-300 bg-rose-200 text-pink-900 focus:border-pink-500 focus:ring-rose-500 rounded-md shadow-sm']) }}>
